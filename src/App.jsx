c4b5ec68e4cedf19b15d7009a5c8461985f6d838
GabrielButarelo <gabrielbutarelo16@gmail.com>
2021-12-08 14:34:01 -0300

    Adicionado componente 'Card'

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
+import './App.css';
+
 import Primeiro from './components/basicos/Primeiro';
 import ComParametro from './components/basicos/ComParametro';
 import Fragmento from './components/basicos/Fragmento';
-import Aleatorio from './components/basicos/Aleatorio'
+import Aleatorio from './components/basicos/Aleatorio';
+
+import Card from './components/layout/Card';
 
 export default _ => 
-    <div id="app">
+    <div className="App">
       <h1>Fundamentos React</h1>
-      <Aleatorio max={100} min={10}/>
-      <Fragmento />
-      <ComParametro titulo="Segundo Componente" aluno="Gabriel" nota={8.5} />
-      <Primeiro />
+      <div className="Cards">
+        <Card titulo="#4 - Desafio Aleatório" color="#080">
+          <Aleatorio max={100} min={10}/>
+        </Card>
+
+        <Card titulo="#3 - Fragmento" color="#008">
+          <Fragmento />
+        </Card>
+
+        <Card titulo="#2 - Parametro" color="#080">
+          <ComParametro titulo="Segundo Componente" aluno="Gabriel" nota={8.5} />
+        </Card>
+
+        <Card titulo="#1 - Primeiro" color="#008">
+          <Primeiro />
+        </Card>
+      </div>
     </div>
